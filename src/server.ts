926eccf60bf93102c4ede742492db30d48619b0e
Ankit Baage <ankitbaage3@gmail.com>
2025-02-13 23:56:54 +0530

    server fixed

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,29 @@
-import express from 'express'
-import router from './router'
-import morgan from 'morgan'
-import cors from 'cors'
-import { protect } from './modules/auth'
-import { createNewUser, signin } from './handlers/user'
+import express from 'express';
+import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
+import { protect } from './modules/auth';
+import { createNewUser, signin } from './handlers/user';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+// Middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res, next) => {
-  setTimeout(() => {
-    next(new Error('hello'))
-  },1)
-})
+// Routes
+app.get('/', (req, res, next) => next(new Error('hello')));
 
-app.use('/api', protect, router)
+app.use('/api', protect, router);
+app.post('/user', createNewUser);
+app.post('/signin', signin);
 
-app.post('/user', createNewUser)
-app.post('/signin', signin)
-
+// Global Error Handler
 app.use((err, req, res, next) => {
-  console.log(err)
-  res.json({message: `had an error: ${err.message}`})
-})
+  console.error(err);
+  res.status(err.statusCode || 500).json({ message: `Error: ${err.message}` });
+});
 
-export default app
+export default app;
